c5f81edd312b1861bc01c7a144e0f9ecb940e055
Kevin Lobo <kevinlobo2@gmail.com>
2020-09-24 21:55:36 -0500

    Changing main.java to accept an arbitrary amount of integers.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,18 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide a few integers to add");
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    private static int addArguments(String[] args) 
+    {
+	int length = args.length;
+	int sum = 0;
+        for(int i = 0; i < length; i++)
+	{
+		sum = sum + Integer.valueOf(args(i));
+	}
+	return sum;
     }
 }
